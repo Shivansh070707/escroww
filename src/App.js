43a8cbbb92345e86869cffb361cb77525eff9715
amanreveation <aman.raj@reveation.io>
2023-04-11 17:50:47 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { Escrow } from './Escrow';
-
+import CssBaseline from '@mui/material/CssBaseline';
 function App() {
   return (
     <div className='App'>
+    <CssBaseline />
       <Escrow />
     </div>
   );
--- src/Escrow.js
+++ src/Escrow.js
@@ -2,6 +2,19 @@ import React, { useState } from 'react';
 import EscrowDetails from './EscrowDetails.js';
 import './Escrow.css';
 import Web3Modal from 'web3modal';
+import Button from '@mui/material/Button';
+import WalletIcon from '@mui/icons-material/Wallet';
+import SendIcon from '@mui/icons-material/Send';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
 // import WalletConnectProvider from '@walletconnect/web3-provider';
 // import WalletLink from 'walletlink';
 const escrowContract = require('./build/polygon/testnet/Escrow/Escrow.json');
@@ -32,7 +45,7 @@ export const Escrow = () => {
   const [account, setaccount] = useState('');
   const [balance, setbalance] = useState('');
   const [address, setaddress] = useState('');
-  const [connect, setconnect] = useState('Connect Wallet');
+  const [connected, setconnected] = useState(false);
   const [hasClicked, setHasClicked] = useState(false);
   const [provider, setprovider] = useState(null);
   const [showForm, setShowForm] = useState(false);
@@ -48,8 +61,10 @@ export const Escrow = () => {
   const [buyertext, setbuyertext] = useState('');
   const [newescrow, setnewescrow] = useState('');
   const [ver, setver] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const connectWallet = async () => {
+    setLoading(true);
     const web3Modal = new Web3Modal();
     const connection = await web3Modal.connect();
     const Provider = new ethers.providers.Web3Provider(connection);
@@ -74,7 +89,6 @@ export const Escrow = () => {
     setaccount(Account);
     const Address = await Account.getAddress();
     setaddress(Address);
-    setconnect('connected :');
     const contract = new ethers.Contract(
       escrowContract.address,
       escrowContract.abi,
@@ -97,6 +111,9 @@ export const Escrow = () => {
     const bal = ethers.utils.formatEther(Bal);
 
     setbalance(`${bal} Matic`);
+    setLoading(false);
+    setconnected(true);
+
   };
 
   const handlerequestChange = (e) => {
@@ -151,12 +168,33 @@ export const Escrow = () => {
   }
 
   return (
-    <div>
-      <u onClick={connectWallet}> {connect} </u>
-      <p>wallet address: {address}</p>
-      <p> user balance : {balance}</p>
+    <div className='home-container'>
+      <Button onClick={connectWallet} variant="contained" endIcon={<WalletIcon />} sx={{ mt:3}}>
+      {connected ? 'Connected' : 'Connect Wallet'}
+      </Button>
+      
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}>
+          <CircularProgress color="inherit" />
+      </Backdrop>
 
-      {!hasClicked && <button onClick={handleClickForm}>Send Tokens</button>}
+      { connected && 
+      <div className='user-info'>
+        <Typography sx={{ mt:3}} variant="h6">
+          Wallet address 
+          <Typography variant='subtitle1'>{address}</Typography>
+        </Typography>
+
+        <Typography sx={{ mt:3, mb:3}} variant="h6">
+          User Balance Address
+          <Typography variant='subtitle1'>{balance}</Typography>
+        </Typography>
+
+        {!hasClicked && <Button variant="contained" endIcon={<SendIcon />} onClick={handleClickForm}>Send Tokens</Button>}
+      </div>
+      }
+
       <div>
         {showForm && (
           <div className='form-container'>
@@ -197,45 +235,50 @@ export const Escrow = () => {
                 />
               </div>
 
-              <button type='submit' className='btn btn-primary'>
+              <Button type='submit' variant="contained">
                 Send Tokens
-              </button>
+              </Button>
+              <Button onClick={()=> {setHasClicked(false); setShowForm(false)}} variant="outlined" sx={{ mt:2}}>Cancel</Button>
             </form>
           </div>
         )}
       </div>
+
       {newescrow ? <EscrowDetails escrow={newescrow} /> : <></>}
 
       {selectedsellerescrow ? (
         <EscrowDetails escrow={selectedsellerescrow} />
       ) : (
-        <ul>
+        <div>
           <h3>{allescrewtext}</h3>
           {allsellerescrow
             .filter((escroww) => escroww.status === CurrentStatus.active)
             .map((escroww, index) => (
-              <li key={index}>
-                <u
-                  onClick={async () => {
+              <Button variant="outlined" key={index} onClick={async () => {
                     const selected = await escrow.getEscrowRequest(
                       Number(escroww.id)
                     );
                     setselectedsellerescrow(selected);
-                  }}
-                >
-                  {`Escrow id: ${Number(escroww.id)}`}
-                </u>
-              </li>
+                  }}>
+                  <b>{`Escrow id: ${Number(escroww.id)}`}</b>
+              </Button>
             ))}
-          {allsellerescrow.filter(
+
+          {connected && allsellerescrow.filter(
             (escroww) => escroww.status === CurrentStatus.active
           ).length === 0 && <p>You have no active transactions </p>}
-        </ul>
+        </div>
       )}
-      <button onClick={handleverify}> Verify Tokens </button>
+
+      <br />
+      { connected &&
+      <Button sx={{ mb:3 }} variant="contained" onClick={handleverify}> Verify Tokens </Button>
+      }
+
       {selectedbuyerescrow ? (
         <EscrowDetails escrow={selectedbuyerescrow} />
       ) : null}
+
       {ver ? (
         <div id='ver'>
           <ul>
@@ -243,18 +286,15 @@ export const Escrow = () => {
             {allbuyerescrows
               .filter((escroww) => escroww.status === CurrentStatus.active)
               .map((escroww, index) => (
-                <li key={index}>
-                  <u
-                    onClick={async () => {
+                <Button sx={{ mb:5}} variant="outlined" key={index} onClick={async () => {
                       const selected = await escrow.getEscrowRequest(
                         Number(escroww.id)
                       );
                       setselectedbuyerescrow(selected);
-                    }}
-                  >
-                    {`Escrow id: ${Number(escroww.id)}`}
-                  </u>
-                </li>
+                    }}>
+                  
+                    <b>{`Escrow id: ${Number(escroww.id)}`}</b>
+                </Button>
               ))}
             {allbuyerescrows.filter(
               (escroww) => escroww.status === CurrentStatus.active
